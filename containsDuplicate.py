36b6299090793f827406303d285ca645151df6c5
Margish <135315187+margish2@users.noreply.github.com>
2025-07-04 15:54:14 -0400

    Update containsDuplicate.py

--- containsDuplicate.py
+++ containsDuplicate.py
@@ -1,1 +1,13 @@
+num = [1,2,3,1]
 
+class containsDuplicate:
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        """
+        Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+        """
+        hash_set = set()
+        for num in nums:
+            if num in hash_set:
+                return True
+            hash_set.add(num)
+        return False
